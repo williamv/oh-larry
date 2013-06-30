242944452ff27b88e68c1d798e712e78f9bab02c
Will Anderson <williamv.anderson@gmail.com>
2013-06-30 11:00:13 -0400

    forgot the train elsif

--- app.rb
+++ app.rb
@@ -24,11 +24,11 @@ def classon_or_stmarks()
 
   prompt; next_move = gets.chomp.downcase
 
-  if next_move.include? "classon"
-  	puts "Good call. You made it to Classon before school starts so it's not crowded."
+  if next_move.include? ("class")
+  	puts "Good call. You made it to Classon before all the kids start going to school."
   	eastern_parkway()
-  elsif next_move.include? ("mark")
-  	puts "Bad move chump. You hit the boneyard and have to dig chicken wings out of his throat."
+  elsif next_move.include? ("mark") or ("st") or ("saint")
+  	puts "Bad move chump. You hit the boneyard and have to dig chicken wings out of his mouth."
   	game_over()
   else
   	clueless()
@@ -36,15 +36,16 @@ def classon_or_stmarks()
 end
 
 def elevated_train
-  puts "You walked right and hear the S train coming. How quickly do you walk (mph)?"
+  puts "You walked right and hear the elevated S train coming. How quickly do you walk (mph)?"
 
   prompt; walking_pace = gets.chomp.to_i
 
   if walking_pace >= 4
-	puts "You hear the woosh of the train behind you."
-	eastern_parkway()
+	 puts "You hear the woosh of the train behind you. Lawrence is calm"
+	 eastern_parkway()
   elsif walking_pace < 4
-	game_over()
+    puts "You walked too slowly! The train is immediately overhead and Lawrence freaks out."
+    game_over()
   else
   	clueless()
   end
@@ -68,13 +69,13 @@ def eastern_parkway()
 end
 
 def mount_prospect()
-  puts "You make it to mount prospect. Did you bring treats?"
+  puts "You make it to the Mount Prospect dog park. Did you bring treats?"
 
   prompt; have_treats = gets.chomp.downcase
 
   if have_treats.include? "y"
-  	puts "You have a great time at the park with the little buddy."
-  	puts "He's a dream on the walk all the way home."
+  	puts "You have a great time at the park with the little buddy and his friends:"
+    new_dog()
   elsif have_treats.include? "n"
   	puts "You have an okay time and only get him back on the leash after he rolls in trash."
   	game_over()
@@ -90,5 +91,21 @@ end
 def game_over()
   puts "The crazy munchkin has to go home. Oh, Larry!"
 end
+
+def new_dog()
+  dog_park_dogs = ['Lawrence','Patty','Cara']
+  for dog in dog_park_dogs
+    puts "#{dog} is at the park with you."
+  end
+
+  puts "A stray joins the pack and you want to name him. What should you call him?"
+  prompt; stray_dog_name = gets.chomp.capitalize
+  dog_park_dogs.push(stray_dog_name)
+  puts "Here are all the dogs here now:"
+
+  for dog in dog_park_dogs
+    puts "#{dog}"
+  end
+end 
 
 start()
